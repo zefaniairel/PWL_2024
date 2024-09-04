a9d67da4b53d5beeb6063e8a96bed69b932980a7
zefania yizreel nefrit sindhoe <zefaniairel@gmail.com>
2024-09-04 09:13:30 +0700

    Pertemuan2-Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () { 
+    return 'Hello World'; 
+ }); 
+
+ Route::get('/world', function () { 
+    return 'World'; 
+ }); 
+
+ Route::get('/selamat', function () { 
+    return 'Selamat Datang'; 
+ }); 
+
+ Route::get('/about', function () { 
+    return 'NIM : 2241760117 Nama : Irel';
+ }); 
+
+ Route::get('/user/{irel}', function ($irel) { 
+    return 'Nama saya '.$irel;
+ }); 
+ 
+ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+ }); 
+
+
+ Route::get('/articles/{id}', function ($idAr) { 
+    return 'Halaman Artikel dengan ID '.$idAr;
+ }); 
+ 
+ Route::get('/user/{name?}', function ($name='irel') { 
+    return 'Nama saya '.$name;
+ }); 
+
+ 
